3401fdb3b355b9018e3982c6bc4fe064019ca812
phungngoc <lam1234@gmail.com>
2024-10-18 23:20:37 +0700

    add file hmtl,css,js

---
+++ js.js
@@ -0,0 +1,7 @@
+function hienthi(){
+    if (document.getElementById('htdm').style.display=="none") {
+        document.getElementById('htdm').style.display="block";
+    }else{
+        document.getElementById('htdm').style.display="none";
+    }
+}
